762f7a31cb24af91a62d0b01c17a28b4a4c30494
Jesús GS <jesalceg@gmail.com>
2020-06-23 18:09:39 -0500

    Dtb

--- proyectocake.sql
+++ proyectocake.sql
@@ -1,13 +1,14 @@
 -- phpMyAdmin SQL Dump
--- version 5.0.2
+-- version 5.0.1
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 20-06-2020 a las 00:47:01
+-- Tiempo de generación: 24-06-2020 a las 00:42:47
 -- Versión del servidor: 10.4.11-MariaDB
--- Versión de PHP: 7.4.5
+-- Versión de PHP: 7.4.3
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -20,6 +21,35 @@ SET time_zone = "+00:00";
 --
 -- Base de datos: `proyectocake`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `clientes`
+--
+
+CREATE TABLE `clientes` (
+  `cl_id` int(11) NOT NULL,
+  `cl_nitempresa` varchar(100) CHARACTER SET utf8 COLLATE utf8_spanish_ci NOT NULL,
+  `cl_nombre` varchar(80) NOT NULL,
+  `cl_direccion` varchar(80) NOT NULL,
+  `cl_email` varchar(80) NOT NULL,
+  `cL_barrio` varchar(80) DEFAULT NULL,
+  `cl_documento` varchar(80) DEFAULT NULL,
+  `cl_fechacrea` datetime DEFAULT NULL,
+  `cl_fechamod` datetime DEFAULT NULL,
+  `cl_usuariocrea` int(11) DEFAULT NULL,
+  `cl_usuariomod` int(11) DEFAULT NULL,
+  `cl_estado` int(11) DEFAULT NULL,
+  `cl_telefono` varchar(80) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `clientes`
+--
+
+INSERT INTO `clientes` (`cl_id`, `cl_nitempresa`, `cl_nombre`, `cl_direccion`, `cl_email`, `cL_barrio`, `cl_documento`, `cl_fechacrea`, `cl_fechamod`, `cl_usuariocrea`, `cl_usuariomod`, `cl_estado`, `cl_telefono`) VALUES
+(1, '654321', '223', '2332', '23', NULL, '233222', NULL, NULL, NULL, NULL, 1, '23');
 
 -- --------------------------------------------------------
 
@@ -105,11 +135,17 @@ INSERT INTO `usuarios` (`us_id`, `us_nitempresa`, `us_usuario`, `us_nombre`, `us
 (2, '654321', 'luis27', 'luis jose', '2704dfe06d48a2c2316dff464f5b7920', 2, 'undefined', 'cl 333', 2, 1, 'luis@gamil.com', '2020-06-19 04:40:11', '2020-06-19 04:40:11', 1, 1, 1),
 (3, '123456', 'crhis27', 'crhitofer p', '7746333f40133ceef6ee5f875e171557', 3, '3302', 'cl 36', 1, 1, 'laverga@gmail.com', '2020-06-19 04:41:29', '2020-06-19 05:03:11', 1, 1, 1),
 (4, '654321', 'jose97', 'jose', '69f84ebe10951c721d165ccf1499ae15', 2, 'undefined', 'cl', 1, 1, 'jose@hotmail.com', '2020-06-19 04:43:29', '2020-06-19 04:43:29', 1, 1, 1),
-(5, '654321', 'juan26', 'juan carlos', '700a6c7459e78d0b0ecbaaead4f91318', 3, '30051025', '', 1, 1, '', '2020-06-19 04:45:01', '2020-06-19 05:13:54', 1, 1, 2);
+(5, '654321', 'juan26', 'juan carlos', '700a6c7459e78d0b0ecbaaead4f91318', 3, '30051025', '', 1, 1, '', '2020-06-19 04:45:01', '2020-06-21 01:27:15', 1, 1, 2);
 
 --
 -- Índices para tablas volcadas
 --
+
+--
+-- Indices de la tabla `clientes`
+--
+ALTER TABLE `clientes`
+  ADD PRIMARY KEY (`cl_id`);
 
 --
 -- Indices de la tabla `empresa`
@@ -132,6 +168,12 @@ ALTER TABLE `usuarios`
 --
 -- AUTO_INCREMENT de las tablas volcadas
 --
+
+--
+-- AUTO_INCREMENT de la tabla `clientes`
+--
+ALTER TABLE `clientes`
+  MODIFY `cl_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT de la tabla `empresa`
